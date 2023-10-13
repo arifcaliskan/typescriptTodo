84a3cfb6bd6871efa99f164965fc716845aca478
Arif Caliskan <85301866+arifcaliskan@users.noreply.github.com>
2023-10-13 15:45:29 +0300

    Models.ts edited

--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,32 @@
+import { useReducer } from "react";
+
 export interface Todo {
   id: number;
   todo: string;
   isDone: boolean;
 }
+
+type Actions =
+  | { type: "add"; payload: string }
+  | { type: "remove"; payload: number }
+  | { type: "done"; payload: number };
+
+const todoReducer = (state: Todo[], action: Actions) => {
+  switch (action.type) {
+    case "add":
+      return [
+        ...state,
+        { id: Date.now(), todo: action.payload, isDone: false },
+      ];
+
+    default:
+      break;
+  }
+};
+
+const model = () => {
+  const [state, dispatch] = useReducer(todoReducer, []);
+  return <div>model</div>;
+};
+
+export default model;
